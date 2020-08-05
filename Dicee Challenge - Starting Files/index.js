513122c80422c38ad307d6367429706901cd7a58
ovyas24 <ovyas24@gmail.com>
2020-08-05 15:13:40 +0530

    project upload

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,23 @@
+function image(){
+  var a = ["images/dice1.png",
+           "images/dice2.png",
+           "images/dice3.png",
+           "images/dice5.png",
+           "images/dice6.png"
+         ];
+  var p1 = Math.random();
+  var p2 = Math.random();
+  p1 = Math.floor(p1*6)+1;
+  p2 = Math.floor(p2*6)+1;
+
+  var winner = document.getElementsByTagName("h1")[0];
+  if (p1>p2) {
+    winner.textContent = "Player 1 won";
+  }else{
+    winner.textContent = "Player 2 won";
+  }
+  document.getElementById("i1").src = a[p1];
+  document.getElementById("i2").src = a[p2];
+}
+
+image();
