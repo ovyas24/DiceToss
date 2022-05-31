28b688ee057149c4bcd4cb7bd64285e01ae09a19
Om Vyas <ovyas24@gmail.com>
2022-05-31 14:53:09 +0530

    Update index.js

--- Dice Toss/index.js
+++ Dice Toss/index.js
@@ -1,17 +1,21 @@
 function image(){
-  var a = ["images/dice1.png",
+  var a = [
+           "images/dice1.png",
            "images/dice2.png",
            "images/dice3.png",
+           "images/dice4.png",
            "images/dice5.png",
            "images/dice6.png"
          ];
   var p1 = Math.random();
   var p2 = Math.random();
-  p1 = Math.floor(p1*6)+1;
-  p2 = Math.floor(p2*6)+1;
+  p1 = Math.floor(p1*6);
+  p2 = Math.floor(p2*6);
 
   var winner = document.getElementsByTagName("h1")[0];
-  if (p1>p2) {
+  if (p1 === p2) {
+    image();
+  }else if (p1>p2) {
     winner.textContent = "Player 1 won";
   }else{
     winner.textContent = "Player 2 won";
